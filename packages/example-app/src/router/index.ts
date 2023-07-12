8518b677daaa9ab743465c5f5215e25f738f0956
Dan Giralté (Ionic) <giralte@ionic.io>
2023-07-12 13:47:48 -0500

    fmt

--- packages/example-app/src/router/index.ts
+++ packages/example-app/src/router/index.ts
@@ -1,24 +1,24 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "../views/HomePage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
-]
+    path: "/home",
+    name: "Home",
+    component: HomePage,
+  },
+];
 
 const router = createRouter({
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
