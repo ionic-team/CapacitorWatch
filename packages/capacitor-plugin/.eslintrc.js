6f3f4bceef010f6f8681763a5122b6f71722a1cb
Dan Giralté (Ionic) <giralte@ionic.io>
2023-07-12 13:09:36 -0500

    fix path to ts config

--- packages/capacitor-plugin/.eslintrc.js
+++ packages/capacitor-plugin/.eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
+        "project": "./tsconfig.json"
     },
     "rules": {
         "no-unused-vars": "off",
